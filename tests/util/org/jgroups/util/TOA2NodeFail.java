b3398b46432db27bfc210a3537e89f8e2845c3aa
Ryan Emerson <ryan.emerson3@gmail.com>
2013-12-03 12:46:25 +0000

    Test class that shows that TOA can fail when only two nodes are present in the total order

---
+++ tests/util/org/jgroups/util/TOA2NodeFail.java
@@ -0,0 +1,73 @@
+package org.jgroups.util;
+
+import org.jgroups.*;
+import org.jgroups.protocols.tom.ToaHeader;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * // TODO: Document this
+ *
+ * @author ryan
+ * @since 4.0
+ */
+public class TOA2NodeFail extends ReceiverAdapter {
+    public static void main(String[] args) {
+        try {
+            new TOA2NodeFail().run("toa.xml");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    private ToaHeader lastHeader = null;
+    private Map<Address, Integer> countRecord = new HashMap<Address, Integer>();
+    private Address localAddress = null;
+    private PrintWriter out = null;
+    private List<ToaHeader> receivedMessages = new ArrayList<ToaHeader>();
+
+    public void run(String propsFile) throws Exception {
+        System.out.println(propsFile);
+        JChannel channel = new JChannel(propsFile);
+        channel.setReceiver(this);
+        channel.connect("uperfBox");
+        localAddress = channel.getAddress();
+
+        try {
+            out = new PrintWriter(new BufferedWriter(new FileWriter("DeliveredMessages" + localAddress + ".csv",
+                    true)), true);
+        } catch (Exception e) {
+        }
+
+        Util.sleep(1000 * 10);
+        int count = 0;
+        while (true) {
+            AnycastAddress anycastAddress = new AnycastAddress(channel.getView().getMembers());
+            Message message = new Message(anycastAddress, new Integer(count));
+            channel.send(message);
+            Util.sleep(1);
+            count++;
+
+            if (count > 20000)
+                break;
+        }
+        System.out.println("Sending finished!");
+
+        Util.sleep(1000 * 20);
+
+        for (ToaHeader h : receivedMessages) {
+            out.println(h);
+        }
+    }
+
+    public void receive(Message msg) {
+        ToaHeader thisHeader = (ToaHeader) msg.getHeader((short) 58);
+        receivedMessages.add(thisHeader);
+    }
+}
