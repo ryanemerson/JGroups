d68daa968dc36120ab9a6da5b2f6d896d9a29a7b
Ryan Emerson <ryan.emerson3@gmail.com>
2014-05-21 11:47:25 +0100

    Updated so that the test is now coordinated using messages not timeouts.  Support multiple sending threads has also been added.

--- src/org/jgroups/protocols/RMSysIntegratedNMC/Test.java
+++ src/org/jgroups/protocols/RMSysIntegratedNMC/Test.java
@@ -1,17 +1,17 @@
 package org.jgroups.protocols.RMSysIntegratedNMC;
 
 import org.jgroups.*;
+import org.jgroups.conf.ClassConfigurator;
 import org.jgroups.protocols.tom.ToaHeader;
 import org.jgroups.util.Util;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * // TODO: Document this
@@ -22,6 +22,7 @@ import java.util.concurrent.Executors;
 public class Test extends ReceiverAdapter {
     public static void main(String[] args) throws Exception{
         String propsFile = "RMSysIntegrated.xml";
+        String initiator = "";
         int numberOfMessages = 100000;
         for (int i = 0; i < args.length; i++) {
             if("-config".equals(args[i])) {
@@ -32,69 +33,150 @@ public class Test extends ReceiverAdapter {
                 numberOfMessages = Integer.parseInt(args[++i]);
                 continue;
             }
+            if("-initiator".equals(args[i])) {
+                initiator = args[++i];
+                continue;
+            }
         }
-        new Test(propsFile, numberOfMessages).run();
+        new Test(propsFile, numberOfMessages, initiator).run();
     }
 
     private final String PROPERTIES_FILE;
     private final int NUMBER_MESSAGES_TO_SEND;
     private final int NUMBER_MESSAGES_PER_FILE = 5000;
-    private final int TIME_BETWEEN_REQUESTS = 1;
+    private final String INITIATOR;
     private final String PATH = "/work/a7109534/";
     private final List<Header> deliveredMessages = new ArrayList<Header>();
     private ExecutorService outputThread = Executors.newSingleThreadExecutor();
-    private Address localAddress;
+    private JChannel channel;
     private int count = 1;
+    private boolean startSending = false;
+    private int completeMsgsReceived = 0;
+    private int msgsReceived = 0;
+    private long startTime;
+    private boolean allMessagesReceived = false;
+    private Future lastOutputFuture;
 
-    public Test(String propsFile, int numberOfMessages) {
+    public Test(String propsFile, int numberOfMessages, String initiator) {
         PROPERTIES_FILE = propsFile;
         NUMBER_MESSAGES_TO_SEND = numberOfMessages;
+        INITIATOR = initiator;
     }
 
     public void run() throws Exception {
-        System.out.println(PROPERTIES_FILE + " | " + NUMBER_MESSAGES_TO_SEND + " messages");
-        final JChannel channel = new JChannel(PROPERTIES_FILE);
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
+        System.out.println(PROPERTIES_FILE + " | " + NUMBER_MESSAGES_TO_SEND + " messages | Initiator " + INITIATOR);
+        channel = new JChannel(PROPERTIES_FILE);
         channel.setReceiver(this);
         channel.connect("uperfBox");
-        localAddress = channel.getAddress();
-        System.out.println("Channel Created | lc := " + localAddress);
+
+        ClassConfigurator.add((short) 1025, TestHeader.class); // Add Header to magic map without adding to file
+        System.out.println("Channel Created | lc := " + channel.getAddress());
         System.out.println("Number of message to send := " + NUMBER_MESSAGES_TO_SEND);
-        System.out.println("Time between each request := " + TIME_BETWEEN_REQUESTS);
 
-        Util.sleep(1000 * 10);
-        int count = 0;
+        Util.sleep(1000 * 30);
+        int sentMessages = 0;
+        startTime = System.nanoTime();
+
+        if (channel.getAddress().toString().contains(INITIATOR))
+            sendStartMessage(channel);
+
         while (true) {
-            AnycastAddress anycastAddress = new AnycastAddress(channel.getView().getMembers());
-            Message message = new Message(anycastAddress, count);
-            channel.send(message);
-            Util.sleep(TIME_BETWEEN_REQUESTS);
-            count++;
+            if (startSending) {
+                AnycastAddress anycastAddress = new AnycastAddress(channel.getView().getMembers());
+                final Message message = new Message(anycastAddress, sentMessages);
+//                threadPool.execute(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        try {
+//                            channel.send(message);
+//                        } catch (Exception e) {
+//                            System.out.println(e);
+//                        }
+//                    }
+//                });
+                channel.send(message);
+                sentMessages++;
 
-            if (count == NUMBER_MESSAGES_TO_SEND)
-                break;
+//                Util.sleep(1);
+
+                if (sentMessages == NUMBER_MESSAGES_TO_SEND)
+                    break;
+            } else {
+                Util.sleep(1);
+            }
         }
-        System.out.println("Sending finished!");
-        Util.sleep(1000 * 60);
-        channel.disconnect();
+        System.out.println("Sending finished! | Time Taken := " + TimeUnit.MILLISECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS));
+
+        while (!allMessagesReceived || channel.getView().size() != completeMsgsReceived || (lastOutputFuture == null || !lastOutputFuture.isDone()))
+            Util.sleep(100);
+
+        System.out.println("Test Finished");
         System.exit(0);
     }
 
+    public void sendStartMessage(JChannel channel) throws Exception {
+        sendStatusMessage(channel, new TestHeader(TestHeader.START_SENDING));
+    }
+
+    private void sendCompleteMessage(JChannel channel) throws Exception {
+        sendStatusMessage(channel, new TestHeader(TestHeader.SENDING_COMPLETE));
+    }
+
+    private void sendStatusMessage(JChannel channel, TestHeader header) throws Exception {
+//        Message message = new Message(new AnycastAddress(channel.getView().getMembers()));
+        Message message = new Message(null);
+        message.putHeader((short) 1025, header);
+        message.setFlag(Message.Flag.NO_TOTAL_ORDER);
+        channel.send(message);
+    }
+
     public void receive(Message msg) {
+        Header header = msg.getHeader((short)1025);
+        if (header != null) {
+            if (((TestHeader)header).type == TestHeader.START_SENDING && !startSending) {
+                startSending = true;
+                System.out.println("Start sending msgs ------");
+            } else {
+                completeMsgsReceived++;
+
+                if (msg.src() != null)
+                    System.out.println("Complete message received from " + msg.src());
+
+                int numberOfNodes = channel.getView().getMembers().size();
+                if (completeMsgsReceived ==  numberOfNodes) {
+                    System.out.println("Cluster finished! | Time Taken := " + TimeUnit.MILLISECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS));
+                }
+            }
+            return;
+        }
+
         synchronized (deliveredMessages) {
             short protocolId = (short) (PROPERTIES_FILE.equalsIgnoreCase("RMSysIntegrated.xml") ? 1008 : 58);
-            Header header = msg.getHeader(protocolId);
+            header = msg.getHeader(protocolId);
             deliveredMessages.add(header);
+
+//            System.out.println("Received msg " + ((RMCastHeader)header).getId());
 
             if (deliveredMessages.size() % NUMBER_MESSAGES_PER_FILE == 0) {
                 final List<Header> outputHeaders = new ArrayList<Header>(deliveredMessages);
                 deliveredMessages.clear();
                 // Output using a single thread to ensure that this operation does not effect receiving messages
-                outputThread.submit(new Runnable() {
+                lastOutputFuture = outputThread.submit(new Runnable() {
                     @Override
                     public void run() {
                         writeHeadersToFile(outputHeaders);
                     }
                 });
+            }
+
+            System.out.println("Msgs received := " + msgsReceived + 1);
+            if (++msgsReceived == (NUMBER_MESSAGES_TO_SEND * channel.getView().size())) {
+                try {
+                    sendCompleteMessage(channel);
+                } catch (Exception e) {
+                }
             }
         }
     }
@@ -103,11 +185,10 @@ public class Test extends ReceiverAdapter {
         System.out.println("New View := " + view);
     }
 
-    private PrintWriter getPrintWriter(Address localAddress, int count) {
+    private PrintWriter getPrintWriter() {
         try {
             new File(PATH).mkdirs();
-
-            return new PrintWriter(new BufferedWriter(new FileWriter(PATH + "DeliveredMessages" + localAddress + "-" + count + ".csv",
+            return new PrintWriter(new BufferedWriter(new FileWriter(PATH + "DeliveredMessages" + channel.getAddress() + "-" + count + ".csv",
                     true)), true);
         } catch (Exception e) {
             System.out.println("Error: " + e);
@@ -116,12 +197,57 @@ public class Test extends ReceiverAdapter {
     }
 
     private void writeHeadersToFile(List<Header> headers) {
-        PrintWriter out = getPrintWriter(localAddress, count);
+        PrintWriter out = getPrintWriter();
         for (Header header : headers)
             if (PROPERTIES_FILE.equalsIgnoreCase("RMSysIntegrated.xml"))
                 out.println(((RMCastHeader) header).getId());
             else if (PROPERTIES_FILE.equalsIgnoreCase("toa.xml"))
                 out.println(((ToaHeader)header).getMessageID());
+        out.flush();
+
+        int numberOfNodes = channel.getView().size();
+        int totalNumberOfRounds = (numberOfNodes * NUMBER_MESSAGES_TO_SEND) / NUMBER_MESSAGES_PER_FILE;
+        if (count == totalNumberOfRounds) {
+            allMessagesReceived = true;
+            System.out.println("&&&&&&&& Final Count := " + count + " | totalNumberOfRounds := " + totalNumberOfRounds +
+                    " | numberOfMessagesToSend := " + NUMBER_MESSAGES_TO_SEND + " | NUMBER_MESSAGES_PER_FILE := " + NUMBER_MESSAGES_PER_FILE);
+        }
+        System.out.println("Count == " + count + " | numberOfRounds := " + totalNumberOfRounds);
         count++;
+    }
+
+    public static class TestHeader extends Header {
+        public static final byte START_SENDING = 1;
+        public static final byte SENDING_COMPLETE = 2;
+
+        private byte type;
+
+        public TestHeader() {}
+
+        public TestHeader(byte type) {
+            this.type = type;
+        }
+
+        @Override
+        public int size() {
+            return Global.BYTE_SIZE;
+        }
+
+        @Override
+        public void writeTo(DataOutput out) throws Exception {
+            out.writeByte(type);
+        }
+
+        @Override
+        public void readFrom(DataInput in) throws Exception {
+            type = in.readByte();
+        }
+
+        @Override
+        public String toString() {
+            return "TestHeader{" +
+                    "type=" + type +
+                    '}';
+        }
     }
 }
