80b2459ba9b40b205d4e57160cd0530f0346cd51
Ryan Emerson <ryan.emerson3@gmail.com>
2014-07-04 16:38:56 +0100

    Script updated so that values in for/if are dynamically assigned based upon array size.

--- testScripts/millDecoupledTerminal.sh
+++ testScripts/millDecoupledTerminal.sh
@@ -2,29 +2,28 @@
 #arr=(csvm0064 csvm0065 csvm0066 csvm0067 csvm0068)
 #boxes=(csvm0067 csvm0068)
 # REMEMBER TO ADD BOX MEMBERS TO DECOUPLED CLASS!!!!!
-arr=(mill002 mill004 mill005 mill006 mill007 mill008 mill009 mill010 mill015 mill016 mill020 mill021 mill058)
-boxes=(mill020 mill021 mill058)
+arr=(mill002 mill005 mill006 mill007 mill008 mill009 mill010 mill012 mill014 mill015 mill016 mill017)
+boxes=(mill016 mill017)
 #outDir="workspace/output/"
 outDir="/work/a7109534/"
 channelName="uperfBox"
 cleanUp="mkdir -p /work/a7109534/; rm /work/a7109534/*;"
 profiler="-agentpath:/home/pg/p11/a7109534/yjp-2013-build-13072/bin/linux-x86-64/libyjpagent.so=monitors"
-#props1="decoupled_hybrid.xml"
-#props2="decoupled_hybrid_box.xml"
-props1="decoupled_TOA.xml"
-props2="decoupled_TOA_Box.xml"
-command1="java -Djava.net.preferIPv4Stack=true $profiler -jar workspace/mperf.jar -props $props1 -boxes"
-command2="java -Djava.net.preferIPv4Stack=true $profiler -jar workspace/jgroups.jar -props $props2 -channel $channelName"
+props1="decoupled_hybrid.xml"
+props2="decoupled_hybrid_box.xml"
+#props1="decoupled_TOA.xml"
+#props2="decoupled_TOA_Box.xml"
+command1="java -Djava.net.preferIPv4Stack=true -jar workspace/mperf.jar -props $props1 -boxes"
+command2="java -Djava.net.preferIPv4Stack=true -jar workspace/jgroups.jar -props $props2 -channel $channelName"
 control=" -control true"
 
 if [[ $# < 1 ]]; then
     for y in ${boxes[@]}; do
         command1=$command1" "$y
     done
-
-    for i in {0..12};do
     
-        if (( $i < 10 )); then
+    for ((i = 0; i < ${#arr[@]}; i++));do
+        if (( $i < (${#arr[@]} - ${#boxes[@]}) )); then
             action=$command1;
         else
             action=$command2;
@@ -54,14 +53,14 @@ else
     if [ $1 = "getOutput" ]; then
        for i in ${arr[@]}; do
             HOST="a7109534@$i"
-            scp a7109534@$i:/work/a7109534/* .
+            scp a7109534@$i:/work/a7109534/mill* .
        done
     fi
     
     if [ $1 = "getBoxOutput" ]; then
        for i in ${boxes[@]}; do
             HOST="a7109534@$i"
-            scp a7109534@$i:/work/a7109534/* .
+            scp a7109534@$i:/work/a7109534/mill* .
        done
     fi
     
