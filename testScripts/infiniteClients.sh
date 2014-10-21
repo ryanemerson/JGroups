31efc03f904d94c4354fbca587ca76071e78f0a9
Ryan Emerson <ryan.emerson3@gmail.com>
2014-10-21 18:51:17 +0100

    If statement fixed, allows the number of messages for an experiment to be correctly specified.

--- testScripts/infiniteClients.sh
+++ testScripts/infiniteClients.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #arr=(csvm0065 csvm0066 csvm0067)
-arr=(mill028 mill029)
+arr=(mill028 mill029 mill030)
 geometry=(109x24+10+40 80x24+1060+40 65x25+5-30 65x25+631-30 65x25+1258-30)
 createDir="mkdir -p /work/a7109534/; rm /work/a7109534/*"
 #config="toa.xml"
@@ -13,7 +13,7 @@ lastNode=$((${#arr[@]} - 1))
 jvmArgs="-Xmx3g"
 outDir="/work/a7109534/"
 for ((i = 0; i < ${#arr[@]}; i++));do
-   if [ $i -eq $lastNode ] && [ $(($totalMessages%2))-eq 0 ]; then
+   if [ $i -eq $lastNode ] && [ $(($totalMessages%2)) -eq 0 ]; then
        let "numberOfMessages+=1"
    fi
    command="java $jvmArgs -Djava.net.preferIPv4Stack=true -agentpath:/home/pg/p11/a7109534/yjp-2013-build-13072/bin/linux-x86-64/libyjpagent.so=monitors -jar workspace/mperf.jar -config $config -nr-messages $numberOfMessages -t-messages $totalMessages -initiator $initiator"
